63b6f34509836d5ebfdc670fa0f2e6df5ab59962
Zander Tedjo <zander.b.tedjo@gmail.com>
2021-04-02 19:26:03 -0500

    Set up initial main structure and display functions

--- Final_Project/Lab5.c
+++ Final_Project/Lab5.c
@@ -24,15 +24,85 @@
 #include "TestMotor.h"
 #include "TestSpeaker.h"
 
-char key;
-uint8_t updateDisplayNeeded = 0;
+//==========================
+// Global variables
+//==========================
 
+#define LOCKED 0
+#define UNLOCKED 1
+#define SET_CODE 2
+uint8_t state = LOCKED;
+
+uint32_t code = 1234;
+uint32_t newCode = 1234;
+uint32_t enteredCode = 1234;
+
+uint8_t updateDisplayNeeded = 1;
+uint8_t clearScreenNeeded = 1;
+
+char key;
+
+
+
+//==========================
+// Helper functions
+//==========================
+
+// Called by scan()
 void recordKey(char k){
 	key = k;
 	updateDisplayNeeded = 1;
 }
 
+//==========================
+// Graphics functions
+//==========================
 
+void clearScreen(void){
+	ST7735_FillScreen(0);
+	clearScreenNeeded = 0;
+}
+
+void drawTitle(void){
+	ST7735_DrawString(0,1," ------------------- ",ST7735_YELLOW);
+	ST7735_DrawString(0,2," |  PANDORA'S BOX  | ",ST7735_YELLOW);
+	ST7735_DrawString(0,3," ------------------- ",ST7735_YELLOW);
+}
+
+
+void drawCode(uint32_t c){
+	uint32_t localCode = c;
+	
+	ST7735_SetCursor(7,6);
+
+	char str[10];
+	sprintf(str, "%d", localCode);						// convert numbers to strings for display
+	ST7735_OutString(str);
+	
+}
+
+void drawLockScreen(){
+	drawTitle();
+
+	ST7735_DrawString(5,5,"Enter Code:",ST7735_YELLOW);
+
+	drawCode(enteredCode);
+}
+
+void drawSetCodeScreen(){
+	
+}
+
+void drawUnlockScreen(){
+	
+}
+
+
+
+
+//==========================
+// Main control loop
+//==========================
 int main(void) { 
   PLL_Init(Bus80MHz);              // bus clock at 80 MHz
 	TExaS_Init(SCOPE_PD2,80000000); 	
@@ -42,16 +112,30 @@ int main(void) {
 	DAC_Init(0x07FF); 							 // init DAC
 	Music_Init();
 	
-	Test_Speaker_Init(); 							// test speaker
-	MotorTest();
+	//Test_Speaker_Init(); 							// test speaker
+	//MotorTest();
 	
   EnableInterrupts();
 
   while(1){
-    if(updateDisplayNeeded){
-			ST7735_SetCursor(0,0);
-			ST7735_OutChar(key);
-			updateDisplayNeeded = 0;
+    if(clearScreenNeeded){
+			clearScreen();
+		}
+		
+		else if(updateDisplayNeeded){
+			if(state == LOCKED){
+				drawLockScreen();
+			}
+			else if(state == UNLOCKED){
+				drawUnlockScreen();
+			}
+			else if(state == SET_CODE){
+				drawSetCodeScreen();
+			}
+		}
+		
+		else{
+			
 		}
 		
   }
